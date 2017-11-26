2ef63375229030c630b92a1c1c37a4d471015acc
pskorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-11-26 14:32:31 +0200

    ref

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,5 @@
-/**
- * Created by Admin on 01.10.2017.
- */
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
 		System.out.println("vnbvbj, ijhkk");
